3ff10f20d3aa661526a30da0951fdcb1b53908d3
fikretgozutok <gozutokfikret@gmail.com>
2025-01-14 02:02:49 +0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const images = document.querySelectorAll(".slider-container img");
 const about = document.querySelector(".about");
 const logoContainer = document.querySelector(".logo-container");
-const amentiesContainer = document.querySelector(".page.amenities");
 const detailPages = document.querySelectorAll(".page.detail");
 
 let currentIndex = 0;
@@ -20,17 +19,10 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.addEventListener("scroll", () => {
-  let amentiesTopSide = amentiesContainer.getBoundingClientRect().top;
-
-  if (amentiesTopSide <= window.innerHeight) {
-    amentiesContainer.classList.add("active");
-  }
-
   detailPages.forEach((page) => {
     let pageTop = page.getBoundingClientRect().top;
     let pageHeight = page.offsetHeight;
 
-    // Sayfa tam görünür olduğunda animasyonları başlat
     if (pageTop <= window.innerHeight && pageTop + pageHeight >= 0) {
       if (!page.classList.contains("active")) {
         page.classList.add("active");
